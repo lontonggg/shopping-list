8593f17d81d1f84d6a0e1c2eee455f866e350e5f
lontonggg <reyhan.zada@ui.ac.id>
2023-09-27 15:54:16 +0700

    Menyelesaikan tutorial 4

--- main/views.py
+++ main/views.py
@@ -22,7 +22,7 @@ def show_main(request):
 
     context = {
         'name': request.user.username,
-        'class': 'PBP A', # Kelas PBP kamu
+        'class': 'PBP C', # Kelas PBP kamu
         'products': products,
         'last_login': request.COOKIES['last_login']
     }
@@ -37,6 +37,8 @@ def create_product(request):
         product.user = request.user
         product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
+    context = {'form': form}
+    return render(request, "create_product.html", context)
 
 def show_xml(request):
     data = Product.objects.all()
@@ -86,3 +88,26 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, id):
+    # Get product berdasarkan ID
+    product = Product.objects.get(pk = id)
+
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk = id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))

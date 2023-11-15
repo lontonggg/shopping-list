fff23544bda783313da945a698ac50a20ac6795f
lontonggg <reyhan.zada@ui.ac.id>
2023-11-15 16:04:31 +0700

    Deploy untuk tutorial 8

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
-from main.views import register, login_user, logout_user, edit_product, delete_product, get_product_json, add_product_ajax
+from main.views import register, login_user, logout_user, edit_product, delete_product, get_product_json, add_product_ajax, create_product_flutter
 
 app_name = 'main'
 
@@ -18,4 +18,5 @@ urlpatterns = [
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
     path('create-product', create_product, name='create_product'),
     path('', show_main, name='show_main'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
